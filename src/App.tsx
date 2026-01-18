c80e83b18f98113ce7f96f7a81161b172063163d
Johny Vilaña <johnyv1305@gmail.com>
2026-01-18 13:17:33 -0500

    Switch to HashRouter for better mobile compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Philosophy from './pages/Philosophy';
 import InsightsPage from './pages/InsightsPage';
